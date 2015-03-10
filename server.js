9d35e17f492971343f3920bef09ac7294ef05099
Eeshan kumar <e3.kumar@gmail.com>
2015-03-10 00:57:36 -0700

    Server working and responding to root with index.html

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
-var server = http.createServer(function(request, response) {
-  console.log(response);
+var server = http.createServer(function(req, res) {
+
+  var pathname = url.parse(req.url).pathname;
+
+  if (pathname === "/") {
+	fs.readFile("./index.html", "utf8", function(err, data) {
+      res.end(data);
+    });
+  } else if (pathname === "/song") {
+	res.end(JSON.stringify(randomIndexValue(stringArray)));
+  } else if (pathname === "/joke") {
+	res.end(JSON.stringify(randomIndexValue(jokes)));
+  } else {
+	res.end("This server only responds to root-url requests");
+  }
 });
 
+server.listen(8080, "localhost");
+console.log("Server Running");
 
-server.listen(8080, 'localhost');
+var stringArray = [
+  "Har Har Har",
+  "Wocka Flocka Docka",
+  "Ting Tang Walla Walla Bing Bang",
+  "Shobedo Op",
+  "Check, check me out",
+  "Hip Hop And You Don\'t Stop"
+];
+
+function randomIndexValue(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var jokes = [
+  { setup: "What's the difference between a guitar and a fish?",
+    punchline: "You can't tuna fish" },
+  { setup: "What do you get when you cross a cow and a duck?",
+    punchline: "Milk and quackers." },
+  { setup: "How many tickles does it take to make an octopus laugh",
+    punchline: "Ten tickles" }
+];
