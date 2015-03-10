5dda96cd04747cdd881dff8b759a4e0aa9c80347
Eeshan kumar <e3.kumar@gmail.com>
2015-03-09 19:04:51 -0700

    Playing around with trying to setup site with http and fs modules

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(request, response) {
+  console.log(response);
+});
+
+
+server.listen(8080, 'localhost');
