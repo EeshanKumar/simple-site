86b1172e8a8325e3476e43c3c8e3e5fea3aeb01e
eeshankumar <e3.kumar@gmail.com>
2015-03-16 22:31:58 -0700

    Added pizzaMe.js, encapsulated modules, revised pigLatin to work with multiple consonants and capitolization

--- app/script.js
+++ app/script.js
@@ -1,49 +1,44 @@
-var stringArray = [
-  "Har Har Har",
-  "Wocka Flocka Docka",
-  "Ting Tang Walla Walla Bing Bang",
-  "Shobedo Op",
-  "Check, check me out",
-  "Hip Hop And You Don\'t Stop"
-];
-
-function randomIndexValue(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-$("button").on("click", function() {
-	var url = $(this).attr("id");
-
-	$.get("/" + url, function(response) {
-		var resText;
-
-		if (typeof response === "object") {
-			resText = response.setup + ": " + response.punchline;
-		} else {
-			resText = response;
-	  }
-	  $("#ajax-" + url).text(resText);
-  });
-});
-
 $(document).ready(function() {
-	//This calls the jquery method get and puts in two arguments. The first is
-	//the URL where the request was sent and the second is an anonymous function
-	//that has an argument of the object or string sent to the url (first argument)
-	$.get( "/person", function( data ) {
-	  //This takes the data object and and puts it as the content of the #person element
-	  $( "#person" ).text("FirstName: " + data.firstName + "  LastName: " + data.lastName);
+	//On button click, do something
+	$("button").on("click", function() {
+		var url = $(this).attr("id");
+
+		//Request and then recieve reponse from server
+		$.get("/" + url, function(response) {
+			var resText;
+
+			//Once received, format response basd on url requested
+			switch (url) {
+				case "song":
+					resText = response;
+					break;
+				case "joke":
+					resText = response.setup + ": " + response.punchline;
+					break;
+				case "pizza":
+					resText = "You get a " + response.crust + " pizza ";
+					resText += "with " + response.cheese + " cheese ";
+					resText += "and " + response.toppings + " on top!";
+					break;
+				default:
+					break;
+			}
+
+		  //Return value in appropriate p tag
+		  $("p#ajax-" + url).text(resText);
+	  });
 	});
-});
 
-$("#piglatin").on("submit", function(e) {
-	e.preventDefault();
-	var firstname = $("input[name=firstname]").val();
-	var lastname = $("input[name=lastname]").val();
-	var name = { firstname: firstname, lastname: lastname };
+	//On piglatin form submit, do something
+	$("#piglatin").on("submit", function(e) {
+		e.preventDefault();
+		var firstname = $("input[name=firstname]").val();
+		var lastname = $("input[name=lastname]").val();
+		var name = { firstname: firstname, lastname: lastname };
 
-	$.post("piglatin", name, function(response) {
-		var piglatinified = response.firstname + " " + response.lastname;
-		$("#piglatinified").text(piglatinified);
+		$.post("piglatin", name, function(response) {
+			var piglatinified = response.firstname + " " + response.lastname;
+			$("#piglatinified").text(piglatinified);
+		});
 	});
 });
--- server.js
+++ server.js
@@ -4,54 +4,32 @@ var bodyparser = require("body-parser");
 var port = process.env.PORT || 3000;
 
 var piglatinify = require("./lib/piglatinify.js");
+var songMe = require("./lib/songMe.js");
+var jokeMe = require("./lib/jokeMe.js");
+var pizzaMe = require("./lib/pizzaMe.js");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/app/"));
 
-var stringArray = [
-  "Har Har Har",
-  "Wocka Flocka Docka",
-  "Ting Tang Walla Walla Bing Bang",
-  "Shobedo Op",
-  "Check, check me out",
-  "Hip Hop And You Don\'t Stop"
-];
-
-function randomIndexValue(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-app.get("/song", function(req, res) {
-  res.send(randomIndexValue(stringArray));
-});
-
-var jokes = [
-  { setup: "What's the difference between a guitar and a fish?",
-    punchline: "You can't tuna fish" },
-  { setup: "What do you get when you cross a cow and a duck?",
-    punchline: "Milk and quackers." },
-  { setup: "How many tickles does it take to make an octopus laugh",
-    punchline: "Ten tickles" }
-];
-
-app.get("/joke", function(req, res) {
-	var myJoke = randomIndexValue(jokes);
-    res.json(myJoke);
-});
-
-//Creates endpoint at /person and setups the
-//anonymous function that is called at that endpoint
-app.get("/person", function(req, res) {
-  //Creates an object
-  var person = { firstName: "John", lastName: "Smith" };
-  //Returns the object via the json method on the res (response) argument
-  res.json(person);
-});
-
 app.get("/", function(req, res) {
   res.sendFile("index.html");
+});
+
+app.get("/song", function(req, res) {
+  var mySong = songMe();
+  res.send(mySong);
+});
+
+app.get("/joke", function(req, res) {
+	var myJoke = jokeMe();
+  res.json(myJoke);
+});
+
+app.get("/pizza", function(req, res) {
+  var myPizza = pizzaMe();
+  res.json(myPizza);
 });
 
 app.post("/piglatin", function(req, res) {
